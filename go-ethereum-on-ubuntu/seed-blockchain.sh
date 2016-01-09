ac1b97ed656ff71f0162920d56d6b2a31fca688b
Marley Gray <marleyg@microsoft.com>
2016-01-09 18:15:30 +0000

    Update seed-blockchain.sh

--- go-ethereum-on-ubuntu/seed-blockchain.sh
+++ go-ethereum-on-ubuntu/seed-blockchain.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-geth --genesis lab-seed.json --port 30303 --networkid 1001001 --rpc --rpccorsdomain "*" console
+geth --genesis lab-seed.json --port 30303 --networkid 1001001 --rpc --rpccorsdomain "*" geth --networkid 1001001 --port 30303 --rpc --rpccorsdomain "*" console --ipcpath "/home/hiro/.ethereum/geth.ipc" console
