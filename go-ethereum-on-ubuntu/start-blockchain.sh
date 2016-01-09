f595eb1e8f4c0ec4b9f71d0189d6acaece594f65
Marley Gray <marleyg@microsoft.com>
2016-01-09 18:14:50 +0000

    Update start-blockchain.sh

--- go-ethereum-on-ubuntu/start-blockchain.sh
+++ go-ethereum-on-ubuntu/start-blockchain.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-geth --networkid 1001001 --port 30303 --rpc --rpccorsdomain "*" console --ipc "/home/hiro/.ethereum/geth.ipc"
+geth --networkid 1001001 --port 30303 --rpc --rpccorsdomain "*" console --ipcpath "/home/hiro/.ethereum/geth.ipc"
