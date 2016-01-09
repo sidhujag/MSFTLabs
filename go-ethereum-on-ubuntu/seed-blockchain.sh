1bdfc269e3d177b96808946a9af82cb4d6932971
Marley Gray <marleyg@microsoft.com>
2016-01-09 16:21:10 -0500

    Update seed-blockchain.sh

--- go-ethereum-on-ubuntu/seed-blockchain.sh
+++ go-ethereum-on-ubuntu/seed-blockchain.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-geth --genesis lab-seed.json --rpc --rpcport "8080" --rpccorsdomain "*" --datadir "/chains/ZeroChain" --port "30303" --nodiscover --ipcapi "admin,db,eth,debug,miner,net,shh,txpool,personal,web3" --rpcapi "db,eth,net,web3" --autodag --networkid 1001001 --nat "any" console 
+geth --genesis lab-seed.json --rpc --rpcport "8080" --rpccorsdomain "*" --datadir "/chains/ZeroChain" --port "30303" --ipcapi "admin,db,eth,debug,miner,net,shh,txpool,personal,web3" --rpcapi "db,eth,net,web3" --autodag --networkid 1001001 --nat "any" console 
