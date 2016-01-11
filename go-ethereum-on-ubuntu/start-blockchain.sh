2cb30698002008c288e89c00b9c2754a00ee2fd6
marleyg <newgate-marleyg@hotmail.com>
2016-01-11 08:07:00 -0500

    Update scripts for new path and specific modules for RPC and IPC

--- go-ethereum-on-ubuntu/start-blockchain.sh
+++ go-ethereum-on-ubuntu/start-blockchain.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-geth --networkid 1001001 --port 30303 --rpc --rpccorsdomain "*" console --ipcpath "/home/hiro/.ethereum/geth.ipc"
+geth --rpc --rpcport "8080" --rpccorsdomain "*" --datadir "chains/ZeroChain" --port "30303" --ipcapi "admin,db,eth,debug,miner,net,shh,txpool,personal,web3" --rpcapi "db,eth,net,web3" --networkid 1001001 console 
+
