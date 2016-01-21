096c9e2b47c83dc1045cfad5f981fe99ac873629
marleyg <newgate-marleyg@hotmail.com>
2016-01-21 14:47:13 -0500

    fixed =

--- artifacts/Add Go Ethereum Node/ssh443.sh
+++ artifacts/Add Go Ethereum Node/ssh443.sh
@@ -1,4 +1,4 @@
 file=/etc/ssh/sshd_config
 cp -p $file $file.old &&
-awk '/Port/ { print; print "Port=443"; next }1' $file > $file.tmp && mv $file.tmp $file
+awk '/Port/ { print; print "Port 443"; next }1' $file > $file.tmp && mv $file.tmp $file
 Port=22
