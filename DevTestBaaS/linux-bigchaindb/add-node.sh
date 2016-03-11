38ba8f20fb5d9911883a37636bf0298409bf2212
Marley Gray <newgate-marleyg@hotmail.com>
2016-03-11 15:08:59 -0500

    Add docker-compose

--- DevTestBaaS/linux-bigchaindb/add-node.sh
+++ DevTestBaaS/linux-bigchaindb/add-node.sh
@@ -20,7 +20,7 @@ echo "vmname: $VMNAME"
 
 # Fetch Genesis and scripts
 cd $HOMEDIR
-wget https://raw.githubusercontent.com/marleyg/MSFTLabs/master/DevTestBaaS/linux-BigchainDB/start-blockchain.sh
+wget https://raw.githubusercontent.com/marleyg/MSFTLabs/master/DevTestBaaS/linux-bigchaindb/start-blockchain.sh
 
 #####################
 # setup the Azure CLI
@@ -29,10 +29,12 @@ time sudo npm install azure-cli -g
 time sudo update-alternatives --install /usr/bin/node nodejs /usr/bin/nodejs 100
 
 ####################
-# Setup BigChainDB
+# Setup Docker & BigChainDB
 ####################
 time sudo apt-get -y install git build-essential libtool autotools-dev autoconf pkg-config libssl-dev libevent-dev bsdmainutils libboost-all-dev libminiupnpc-dev libzmq3-dev
 time sudo apt-get install -y software-properties-common
+time sudo curl -L https://github.com/docker/compose/releases/download/1.6.2/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+time sudo chmod +x /usr/local/bin/docker-compose
 time sudo git clone https://github.com/bigchaindb/bigchaindb
 time cd bigchaindb
 time sudo docker-compose build
